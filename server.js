a182e5a040c77ba5df9e82f9c0c44e2e39b5ebe1
arunjacob1729 <arunjacob1729@gmail.com>
2017-08-09 20:52:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('article-one',function(req, res){
+   res.sendFile("article one will be here");
+});
+
+app.get('article-two',function(req, res){
+   res.sendFile("article two will be here");
+});
+
+
+app.get('article-three',function(req, res){
+   res.sendFile("article three will be here");
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
