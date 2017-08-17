ed9f2c70ad3315403327448622f3e21465ddbccf
arunjacob1729 <arunjacob1729@gmail.com>
2017-08-17 20:24:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,12 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML='Hello Dude';
+var img = document.getElementByID('madi')
+function moveleft(){
+    var margin= margin- 1;
+   img.style. margin= margin +"px";
+}
+
+img.onClick =function(){
+var inertval =setInterval(moveLeft,100)
+}
